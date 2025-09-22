61200f2b8154879994a6655eb6fd6ce32ef97dca
nurbek <kubatbek_n@auca.kg>
2025-09-22 18:29:46 +0600

    add theater migration

---
+++ DAL/Migrations/20250919191608_AddedTheaterLogo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTheaterLogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Theaters",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Theaters");
+        }
+    }
+}
